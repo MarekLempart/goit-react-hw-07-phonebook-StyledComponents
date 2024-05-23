4350b0cfa80d4bf41139490c7f5f2455afd67e8b
MarekLempart <kama.zakupy@gmail.com>
2024-05-23 14:25:20 +0200

    Update operations.js

--- src/redux/operations.js
+++ src/redux/operations.js
@@ -6,7 +6,7 @@ import axios from 'axios';
 import { createAsyncThunk } from '@reduxjs/toolkit';
 
 // Ustawiamy bazowy URL dla wszystkich żądań axios
-axios.defaults.baseURL = 'https://64525eefbce0b0a0f744d446.mockapi.io'; // osobisty mock-serwer
+axios.defaults.baseURL = 'https://664f3485fafad45dfae2ccde.mockapi.io'; // osobisty mock-serwer
 
 // Definiujemy asynchroniczną akcję do pobierania kontaktów
 export const fetchContacts = createAsyncThunk(
